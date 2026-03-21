7ec00cfa25e617a6731a4ce88b217518b0cceb1f
vemulaharshavardhangoud <harshavardhan277623@gmail.com>
2026-03-21 17:21:22 +0530

    Fix GitHub Pages Routing by using HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 
 import Sidebar from './components/Sidebar';
